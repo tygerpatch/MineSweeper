967077a20d4930d47804e86f59cdc2f52505ce68
Todd Gerspacher <toddgerspacher27@gmail.com>
2012-12-21 20:51:08 -0500

    added splatter method, to splatter grid with mines

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,7 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JButton;
@@ -14,6 +15,99 @@ public class Client extends JPanel implements ActionListener {
 	final int NUM_COLUMNS = 9;
 
 	char grid[][] = new char[NUM_ROWS][NUM_COLUMNS];
+
+	int NUM_MINES = 10;
+
+	// method to "splatter" grid with mines
+	private void splatter() {
+		int row, column;
+		int count = NUM_MINES;
+
+		Random random = new Random();
+		// Where I learned about using Random
+		// http://www.javapractices.com/topic/TopicAction.do?Id=62
+
+		while(count > 0) {
+			// get random location on grid
+			row = random.nextInt(NUM_ROWS);
+			column = random.nextInt(NUM_COLUMNS);
+
+			// prevent mines from being placed on top of other mines
+			// ensure their is a neighboring cell with a number in it
+			if(hasNeighboringCounter(row, column)) {
+				updateNeighboringCounters(row, column);
+				grid[row][column] = 'M';
+				count--;
+			}
+		}
+	}
+
+	private void updateNeighboringCounters(int row, int column) {
+		updateCounter(row - 1, column - 1);
+		updateCounter(row - 1, column);
+		updateCounter(row - 1, column + 1);
+		updateCounter(row, column - 1);
+		updateCounter(row, column + 1);
+		updateCounter(row + 1, column - 1);
+		updateCounter(row + 1, column);
+		updateCounter(row + 1, column + 1);
+	}
+
+	private void updateCounter(int row, int column){
+		if(isOnGrid(row, column)){
+			if(' ' == grid[row][column]) {
+				grid[row][column] = '1';
+			}
+			else if('1' == grid[row][column]) {
+					grid[row][column] = '2';
+			}
+			else if('2' == grid[row][column]) {
+					grid[row][column] = '3';
+			}
+			else if('3' == grid[row][column]) {
+					grid[row][column] = '4';
+			}
+			else if('4' == grid[row][column]) {
+					grid[row][column] = '5';
+			}
+			else if('5' == grid[row][column]) {
+					grid[row][column] = '6';
+			}
+			else if('6' == grid[row][column]) {
+					grid[row][column] = '7';
+			}
+			else if('7' == grid[row][column]) {
+					grid[row][column] = '8';
+			}
+		}
+	}
+
+	private boolean hasNeighboringCounter(int row, int column) {
+		return isNeighboringCounter(row - 1, column - 1) ||
+				isNeighboringCounter(row - 1, column) ||
+				isNeighboringCounter(row - 1, column + 1) ||
+				isNeighboringCounter(row, column - 1) ||
+				isNeighboringCounter(row, column + 1) ||
+				isNeighboringCounter(row + 1, column - 1) ||
+				isNeighboringCounter(row + 1, column) ||
+				isNeighboringCounter(row + 1, column + 1);
+	}
+
+	private boolean isNeighboringCounter(int row, int column){
+		if(isOnGrid(row, column)) {
+			// basically looking for anything but a mine
+			return ('M' != grid[row][column]);
+		}
+
+		return false;
+	}
+
+	private boolean isOnGrid(int row, int column){
+		boolean isValidRow = ((row >= 0) && (row < NUM_ROWS));
+		boolean isValidColumn = ((column >= 0) && (column < NUM_COLUMNS));
+
+		return (isValidRow && isValidColumn);
+	}
 
 	private void clearGrid(){
 		for(int row = 0; row < NUM_ROWS; row++){
@@ -65,7 +159,6 @@ public class Client extends JPanel implements ActionListener {
 				checkNeighbor(row + 1, column + 1, rows, columns);
 			}
 	}
-
 
 	private void checkNeighbor(int row, int column, Stack<Integer> rows, Stack<Integer> columns){
 		// test for index out of range
