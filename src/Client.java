5c7e3208756b28249710a37cd2a875f94f4129ca
Todd Gerspacher <toddgerspacher27@gmail.com>
2012-12-22 20:21:14 -0500

    fixed bug

--- src/Client.java
+++ src/Client.java
@@ -19,8 +19,6 @@ public class Client extends JPanel implements ActionListener {
 	private char grid[][] = new char[NUM_ROWS][NUM_COLUMNS];
 
 	public Client() {
-		System.out.println("~ start ~");
-
 		// start with just a 9x9 grid with 10 mines
 		// (later allow user to specify these settings)
 		this.setLayout(new GridLayout(9, 9, 10, 10));
@@ -30,8 +28,8 @@ public class Client extends JPanel implements ActionListener {
 
 		JButton button;
 
-		for(int row = 0; row < NUM_ROWS; row++){
-			for(int column = 0; column < NUM_COLUMNS; column++){
+		for(int row = 0; row < NUM_ROWS; row++) {
+			for(int column = 0; column < NUM_COLUMNS; column++) {
 				grid[row][column] = ' ';	// empty square
 			}
 		}
@@ -40,114 +38,149 @@ public class Client extends JPanel implements ActionListener {
 		splatter();
 
 		for(int row = 0; row < NUM_ROWS; row++) {
-			for(int column = 0; column < NUM_COLUMNS; column++){
+			for(int column = 0; column < NUM_COLUMNS; column++) {
 				button = new Cell("" + grid[row][column], row, column);
 				// TODO: Cell should display what's on the grid at that location only when the user clicks on it
 				button.addActionListener(this);
 				this.add(button);
 			}
 		}
-
-		System.out.println("~ end ~");
 	}
 
+	// method to "splatter" grid with mines
 	private void splatter() {
+		// temporarily store location of placed mines
+		int rows[] = new int[NUM_MINES];
+		int columns[] = new int[NUM_MINES];
+
+		// variables to hold a particular mine's grid location
 		int row, column;
-		int count = NUM_MINES;
 
 		Random random = new Random();
 		// Where I learned about using Random
 		// http://www.javapractices.com/topic/TopicAction.do?Id=62
 
-		while(count > 0) {
-			// get random location on grid
+		// variable used in checking if mine was placed at a valid location
+		boolean valid;
+
+		for(int count = 0; count < NUM_MINES; count++) {
+			// get random grid location
 			row = random.nextInt(NUM_ROWS);
 			column = random.nextInt(NUM_COLUMNS);
 
+			// remember grid location
+			rows[count] = row;
+			columns[count] = column;
+
 			// prevent mines from being placed on top of other mines
-			// ensure their is a neighboring cell with a number in it
-			if(hasNeighboringCounter(row, column)) {
-				updateNeighboringCounters(row, column);
+			if('M' != grid[row][column]) {
 				grid[row][column] = 'M';
+			}
+			else {
+				// repeat placement of mine at random location
+				count--;
+			}
+		}
+
+		for(int count = 0; count < NUM_MINES; count++) {
+			// assume placed mine was invalid (even though it should rarely occur)
+			// that is, it doesn't have a neighboring cell with a number in it
+			valid = false;
+
+			// recall mine's grid location
+			row = rows[count];
+			column = columns[count];
+
+			// if at least one neighboring cell has it's mine count updated, then mine's location was valid
+			valid = updateCounter(row - 1, column - 1) || valid;
+			valid = updateCounter(row - 1, column) || valid;
+			valid = updateCounter(row - 1, column + 1) || valid;
+			valid = updateCounter(row, column - 1) || valid;
+			valid = updateCounter(row, column + 1) || valid;
+			valid = updateCounter(row + 1, column - 1) || valid;
+			valid = updateCounter(row + 1, column) || valid;
+			valid = updateCounter(row + 1, column + 1) || valid;
+
+			// Note: valid variable is placed as second operand of OR operator to prevent short-circuiting.
+			// That is, if valid is true then updateCounter would not be called.
+			// Following the same logic, separate OR statements were used as well.
+
+			if(!valid) {
+				// remove invalid mine
+				grid[row][column] = ' ';
+
+				// get next mine's location
+				row = rows[count + 1];
+				column = columns[count + 1];
+
+				// prevent mines from being placed on top of other mines
+				while('M' == grid[row][column]) {
+					// get random grid location
+					row = random.nextInt(NUM_ROWS);
+					column = random.nextInt(NUM_COLUMNS);
+				}
+
+				// remember grid location
+				rows[count] = row;
+				columns[count] = column;
+
+				// repeat validation check
 				count--;
 			}
 		}
 	}
 
-	private boolean hasNeighboringCounter(int row, int column) {
-		return isNeighboringCounter(row - 1, column - 1) ||
-				isNeighboringCounter(row - 1, column) ||
-				isNeighboringCounter(row - 1, column + 1) ||
-				isNeighboringCounter(row, column - 1) ||
-				isNeighboringCounter(row, column + 1) ||
-				isNeighboringCounter(row + 1, column - 1) ||
-				isNeighboringCounter(row + 1, column) ||
-				isNeighboringCounter(row + 1, column + 1);
-	}
-
-	private boolean isNeighboringCounter(int row, int column){
-		if(isOnGrid(row, column)) {
-			// basically looking for anything but a mine
-			return ('M' != grid[row][column]);
-		}
-
-		return false;
-	}
-
-	private boolean isOnGrid(int row, int column){
+	private boolean isOnGrid(int row, int column) {
 		boolean isValidRow = ((row >= 0) && (row < NUM_ROWS));
 		boolean isValidColumn = ((column >= 0) && (column < NUM_COLUMNS));
 
 		return (isValidRow && isValidColumn);
 	}
 
-	private void updateNeighboringCounters(int row, int column) {
-		updateCounter(row - 1, column - 1);
-		updateCounter(row - 1, column);
-		updateCounter(row - 1, column + 1);
-		updateCounter(row, column - 1);
-		updateCounter(row, column + 1);
-		updateCounter(row + 1, column - 1);
-		updateCounter(row + 1, column);
-		updateCounter(row + 1, column + 1);
-	}
-
-	private void updateCounter(int row, int column){
+	private boolean updateCounter(int row, int column) {
 		if(isOnGrid(row, column)) {
-			System.out.print("Updating Count @(" + row + ", " + column + ") ");
 			if(' ' == grid[row][column]) {
-				System.out.println("1");
 				grid[row][column] = '1';
+				return true;
 			}
-			else if('1' == grid[row][column]) {
-				System.out.println("2");
-					grid[row][column] = '2';
+
+			if('1' == grid[row][column]) {
+				grid[row][column] = '2';
+				return true;
 			}
-			else if('2' == grid[row][column]) {
-				System.out.println("3");
-					grid[row][column] = '3';
+
+			if('2' == grid[row][column]) {
+				grid[row][column] = '3';
+				return true;
 			}
-			else if('3' == grid[row][column]) {
-				System.out.println("4");
-					grid[row][column] = '4';
+
+			if('3' == grid[row][column]) {
+				grid[row][column] = '4';
+				return true;
 			}
-			else if('4' == grid[row][column]) {
-				System.out.println("5");
-					grid[row][column] = '5';
+
+			if('4' == grid[row][column]) {
+				grid[row][column] = '5';
+				return true;
 			}
-			else if('5' == grid[row][column]) {
-				System.out.println("6");
-					grid[row][column] = '6';
+
+			if('5' == grid[row][column]) {
+				grid[row][column] = '6';
+				return true;
 			}
-			else if('6' == grid[row][column]) {
-				System.out.println("7");
-					grid[row][column] = '7';
+
+			if('6' == grid[row][column]) {
+				grid[row][column] = '7';
+				return true;
 			}
-			else if('7' == grid[row][column]) {
-				System.out.println("8");
-					grid[row][column] = '8';
+
+			if('7' == grid[row][column]) {
+				grid[row][column] = '8';
+				return true;
 			}
 		}
+
+		return false;
 	}
 
 	@Override
